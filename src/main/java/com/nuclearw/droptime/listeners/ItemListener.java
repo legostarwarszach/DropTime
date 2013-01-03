517f6ca2974969dbfca4af1f85d9d52120edf827
NuclearW <incongruency@gmail.com>
2013-01-03 09:36:40 -0500

    Loop all the things

--- src/main/java/com/nuclearw/droptime/listeners/ItemListener.java
+++ src/main/java/com/nuclearw/droptime/listeners/ItemListener.java
@@ -1,9 +1,14 @@
 package com.nuclearw.droptime.listeners;
 
+import java.util.ArrayList;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.event.world.ChunkUnloadEvent;
 import org.bukkit.inventory.ItemStack;
 
 import com.nuclearw.droptime.Config;
@@ -23,9 +28,28 @@ public class ItemListener implements Listener {
 		}
 	}
 
-/*
+	@EventHandler
+	public void onChunkUnload(ChunkUnloadEvent event) {
+		ArrayList<Item> items = new ArrayList<Item>();
+
+		for(Entity entity : event.getChunk().getEntities()) {
+			if(entity instanceof Item) {
+				items.add((Item) entity);
+			}
+		}
+
+		for(ItemStack itemStack : plugin.items.keySet()) {
+			for(Item item : items) {
+				if(item.getItemStack().equals(itemStack)) {
+					event.setCancelled(true);
+					return;
+				}
+			}
+		}
+	}
+
 	@EventHandler
 	public void onItemPickup(PlayerPickupItemEvent event) {
+		plugin.items.remove(event.getItem().getItemStack());
 	}
-*/
 }
--- src/main/java/com/nuclearw/droptime/runnables/ItemRemoverTask.java
+++ src/main/java/com/nuclearw/droptime/runnables/ItemRemoverTask.java
@@ -1,5 +1,12 @@
 package com.nuclearw.droptime.runnables;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
 import org.bukkit.inventory.ItemStack;
 
 import com.nuclearw.droptime.DropTime;
@@ -13,13 +20,41 @@ public class ItemRemoverTask implements Runnable {
 
 	@Override
 	public void run() {
-		for(ItemStack key : plugin.items.keySet()) {
-			if(plugin.items.get(key) == 0) {
-				key.setAmount(0);
-				plugin.items.remove(key);
-			} else {
+		ArrayList<ItemStack> remove = new ArrayList<ItemStack>();
+
+		Iterator<ItemStack> iterator = plugin.items.keySet().iterator();
+
+		while(iterator.hasNext()) {
+			ItemStack key = iterator.next();
+
+			if(plugin.items.get(key) != null && plugin.items.get(key) == 0) {
+				remove.add(key);
+				iterator.remove();
+			} else if(plugin.items.get(key) != null) {
 				int newTime = plugin.items.get(key) - 1;
 				plugin.items.put(key, newTime);
+			} else {
+				plugin.getLogger().warning("Encoutered a situation I have no idea what I am doing in.  Save me.");
+			}
+		}
+
+		if(remove.size() > 0) {
+			ArrayList<Item> items = new ArrayList<Item>();
+
+			for(World world : Bukkit.getWorlds()) {
+				for(Entity entity: world.getEntities()) {
+					if(entity instanceof Item) {
+						items.add((Item) entity);
+					}
+				}
+			}
+
+			for(ItemStack itemStack : remove) {
+				for(Item item : items) {
+					if(item.getItemStack().equals(itemStack)) {
+						item.remove();
+					}
+				}
 			}
 		}
 	}
